4eba397bc5bccc1acfd2733814cc492ac3e4ab23
otaviobleich <ferreiraotavio577@gmail.com>
2024-08-21 20:46:23 -0300

    Criação do projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "João Pedro";
+
+        /*
+        *   Operadores Logicos
+        *   >  Maior
+        *   >= Maior igual
+        *   <  Menor
+        *   <= Menor Igual
+        *   == é Igual
+        *   != é diferente
+        *   equal Compara Textos
+        * */
+
+        if(idadePessoa > 21) {
+            System.out.println("é maior que 21");
+        }else{
+            System.out.println("Não é maior");
+
     }
 }
